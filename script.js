e41cdb6fb6f0526187256a484a04f0a99ecafc07
Andrii Provozin <andrii.provozin@alexatranslations.com>
2021-05-13 11:44:17 -0500

    complete

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result_deposit").innerText = principal + ",";
+    document.getElementById("result_rate").innerText = rate + "%.";
+    document.getElementById("result_amount").innerText = parseFloat(interest) + ",";
+    document.getElementById("result_year").innerText = year;
+
+    if (parseInt(principal) <= 0) {
+        confirm("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
